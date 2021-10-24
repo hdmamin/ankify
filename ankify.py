9a4ce54405684263cb4acdf38a0b3ce936c27146
hdmamin <hmamin2@dons.usfca.edu>
2021-10-24 15:46:53 -0700

    add sample file

--- ankify.py
+++ ankify.py
@@ -1,14 +1,22 @@
+"""
+Examples
+--------
+ankify ~/notes/dl/gans.txt
+ankify ~/notes/dl
+"""
+
 import fire
 from functools import partial
 import pandas as pd
 from pathlib import Path
-from tqdm.auto import tqdm
 
-# from htools.cli import fire, module_docstring
 from htools.core import load, parallelize
 
 
 def process_one_file(path, out_dir, sep):
+    """Extract q/a pairs from a single txt file and save it as a csv. Args are
+    the same as the ones documented in `main`.
+    """
     path = Path(path)
     text = load(path)
     pairs = []
@@ -33,6 +41,25 @@ def process_one_file(path, out_dir, sep):
 
 
 def main(path, out_dir='/tmp', sep='\t', chunksize=1):
+    """Convert notes (in the form of 1 or more txt files) to csv's that can be
+    imported into anki.
+
+    Parameters
+    ----------
+    path: str or Path
+        Text file or directory containing multiple text files. Any file
+        containing Q/A pairs must use my standard Anki formatting
+        (see data/sample_notes.txt).
+    sep: str
+        Used to separate fields in resulting csv/tsv. I rarely (never?) use
+        tabs in my notes since vim converts them to spaces. Haven't tested
+        behavior if this char appears in your text file naturally.
+    chunksize: int
+        When passing in a directory for `path`, this is a parameter that
+        controls how many items multiprocessing sends to each process. Usually
+        1 should be fine but if you have a directory with thousands of files
+        you might try a larger number.
+    """
     path = Path(path)
     func = partial(process_one_file, out_dir=out_dir, sep=sep)
     if path.is_file():
